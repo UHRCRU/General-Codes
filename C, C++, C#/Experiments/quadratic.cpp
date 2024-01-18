e49c3a8a15f9e9c10cf6e284a1430daabda16cc9
UHRCRU <uhrcru01@gmail.com>
2024-01-18 12:35:17 +0300

    Update quadratic.cpp

--- C, C++, C#/Experiments/quadratic.cpp
+++ C, C++, C#/Experiments/quadratic.cpp
@@ -6,16 +6,13 @@ int h[TABLE_SIZE]={NULL};
 
 void insert()
 {
-
  int key,index,i,flag=0,hkey;
  printf("\nenter a value to insert into hash table\n");
  scanf("%d",&key);
  hkey=(2*key+3)%TABLE_SIZE;
  for(i=0;i<TABLE_SIZE;i++)
 {
-
      index=(hkey+i*i)%TABLE_SIZE;
-
      if(h[index] == NULL)
      {
         h[index]=key;
@@ -27,7 +24,6 @@ if(i == TABLE_SIZE)
 }
 void search()
 {
-
  int key,index,i,flag=0,hkey;
  printf("\nenter search element\n");
  scanf("%d",&key);
@@ -47,13 +43,9 @@ void search()
 void display()
 {
   int i;
-
   printf("\nelements in the hash table are \n");
-
   for(i=0;i< TABLE_SIZE; i++)
-
               printf("\nat index %d \t value =  %d",i,h[i]);
-
 }
 main()
 {
