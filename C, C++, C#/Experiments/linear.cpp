a9914519706037ad30bd55f52b57f6707a1834c5
UHRCRU <uhrcru01@gmail.com>
2024-01-18 12:33:23 +0300

    Update linear.cpp

--- C, C++, C#/Experiments/linear.cpp
+++ C, C++, C#/Experiments/linear.cpp
@@ -6,31 +6,24 @@ int h[TABLE_SIZE]={NULL};
 
 void insert()
 {
-
  int key,index,i,flag=0,hkey;
  printf("\nenter a value to insert into hash table\n");
  scanf("%d",&key);
  hkey=(2*key+3)%TABLE_SIZE;
  for(i=0;i<TABLE_SIZE;i++)
     {
-
      index=(hkey+i)%TABLE_SIZE;
-
      if(h[index] == NULL)
      {
         h[index]=key;
          break;
      }
-
     }
-
     if(i == TABLE_SIZE)
-
      printf("\nelement cannot be inserted\n");
 }
 void search()
 {
-
  int key,index,i,flag=0,hkey;
  printf("\nenter search element\n");
  scanf("%d",&key);
@@ -49,15 +42,10 @@ void search()
 }
 void display()
 {
-
   int i;
-
   printf("\nelements in the hash table are \n");
-
   for(i=0;i< TABLE_SIZE; i++)
-
   printf("\nat index %d \t value =  %d",i,h[i]);
-
 }
 main()
 {
